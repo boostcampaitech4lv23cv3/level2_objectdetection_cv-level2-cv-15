23250e7475e71d957b2ab84a6b8218a70a4880f7
SS-hj <gkwjd111@naver.com>
2022-11-23 07:23:54 +0000

    [Feat] swin_base bifpn

--- mmdetection/configs/_trash_/_base_/default_runtime.py
+++ mmdetection/configs/_trash_/_base_/default_runtime.py
@@ -1,14 +1,14 @@
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(interval=30)
 # yapf:disable
 log_config = dict(
-    interval=50,
+    interval=100,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='WandbLoggerHook', interval=10, # WandB Config
              init_kwargs=dict(
                 project='Trash Object Detection',
                 entity = 'fullhouse',
-                name = 'YM_SWIN_FocalLoss' # 실험할때마다 RUN에 찍히는 이름
+                name = 'HJ_cascade_swin_base_bifpn_hard-aug_CosineAnnealing' # 실험할때마다 RUN에 찍히는 이름
             ),
             )
     ])
--- mmdetection/configs/_trash_/_base_/schedules/schedule_1x.py
+++ mmdetection/configs/_trash_/_base_/schedules/schedule_1x.py
@@ -1,11 +1,20 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
-# learning policy
+optimizer = dict(
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
 lr_config = dict(
-    policy='step',
+    policy='CosineAnnealing',
     warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+    warmup_iters=250,
+    warmup_ratio=1.0 / 10,
+    min_lr_ratio=1e-5)
+runner = dict(type='EpochBasedRunner', max_epochs=30)
